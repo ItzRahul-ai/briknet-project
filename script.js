2354ef66e540e2f241bcb205f85df3d9df0d8acc
Mr_Rahul <dipbag009@gmail.com>
2025-11-22 11:38:37 +0530

    complete responsive navbar

---
+++ script.js
@@ -0,0 +1,26 @@
+// mobile menu toggle (with height animation)
+document.getElementById("btn").addEventListener("click", () => {
+
+  const menu = document.getElementById("mobile-menu");
+
+  if (menu.classList.contains("hidden")) {
+
+    menu.classList.remove("hidden");
+
+    menu.style.height = "0px";
+
+    let fullHeight = "250px";
+
+    requestAnimationFrame(() => {
+      menu.style.height = fullHeight;
+    });
+
+  } else {
+
+    menu.style.height = "0px";
+
+    setTimeout(() => {
+      menu.classList.add("hidden");
+    }, 280);
+  }
+});
